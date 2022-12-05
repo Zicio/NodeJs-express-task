c8901632a0bbc25733380efd220d620b47c68f2c
Alexander Burachkov <zicio@yandex.ru>
2022-12-05 23:44:12 +0400

    worked on ejs

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import router from "./routers/router";
+import bookRouter from "./routers/bookRouter";
 
 dotenv.config({ path: __dirname + "/.env" });
 
 const app = express();
 
+app.set("view engine", "ejs");
 app.use(express.json());
-app.use(router);
+app.use(bookRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
