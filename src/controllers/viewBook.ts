fa548edfa68b860ac275c7d6a25e66438a56c3d9
Alexander Burachkov <zicio@yandex.ru>
2022-12-21 21:18:34 +0400

    Solved

--- src/controllers/viewBook.ts
+++ src/controllers/viewBook.ts
@@ -5,6 +5,11 @@ import path from "path";
 
 dotenv.config({ path: path.join(__dirname, "../../config/.env") });
 
+interface IValueOfCounter {
+  message: string;
+  counter?: string;
+}
+
 const viewBook = async (req: Request, res: Response) => {
   const { id } = req.params;
   const idx = library.findIndex((el) => el.id === id);
@@ -14,17 +19,24 @@ const viewBook = async (req: Request, res: Response) => {
     return;
   }
 
-  const incrCounter = await fetch(process.env.COUNTER_URL + `${id}/incr`, {
-    method: "POST",
-    body: JSON.stringify("Увеличить"),
-  });
-  const counter = await fetch(process.env.COUNTER_URL + `${id}`);
-  console.log(incrCounter.json());
-  console.log(counter.json());
+  let view = 0;
+  try {
+    const incrCounter = await fetch(`${process.env.COUNTER_URL}/${id}/incr`, {
+      method: "POST",
+      body: JSON.stringify({ task: "Увеличить" }),
+    });
+    const counter = await fetch(`${process.env.COUNTER_URL}/${id}`);
+    const incr = await incrCounter.json();
+    const valueOfCounter: IValueOfCounter = await counter.json();
+    view = +(valueOfCounter.counter as string);
+  } catch (e) {
+    console.log(e);
+  }
 
   res.render("views/book/view", {
     title: "Book | view",
     book: library[idx],
+    view: view,
   });
 };
 
