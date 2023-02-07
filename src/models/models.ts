85f786fd213e57fa79cf18e8b27360fb91cf0cb8
Alexander Burachkov <zicio@yandex.ru>
2023-02-07 19:59:50 +0400

    HomeWork 1.2 Typescript - solved

--- src/models/models.ts
+++ src/models/models.ts
@@ -13,3 +13,27 @@ export interface IUser {
   name: string;
   password: string;
 }
+
+
+// Задание №3
+abstract class BooksRepository {
+  createBook(book: IBook): void {
+    console.log("Книга создана");
+  }
+
+  getBook(id: string): IBook{
+    return {title: "Witcher", description: "Polish fantasy", authors: "Andrzej Sapkowski"}
+  }
+
+  getBooks():IBook[] {
+    return [{title: "Witcher", description: "Polish fantasy", authors: "Andrzej Sapkowski"}, {title: "Star Wars", description: "American fantastic", authors: "George Lucas"}]
+  }
+
+  updateBook(id: string): void{
+    console.log("Книга обновлена");
+  }
+
+  deleteBook(id: string): void{
+    console.log("Книга удалена");
+  }
+}
